7bfb909fff5265aee9dc3ab11874a0cf8c3f76e7
Marat Abrarov <abrarov@gmail.com>
2016-05-06 22:01:43 +0300

    Windows batch-scripts for building of Boost (shared / static / static with static C/C++ runtime) for MSVC 2008-2015.

---
+++ boost/build-msvc9-amd64-win64.bat
@@ -0,0 +1,33 @@
+@echo off
+
+rem -------------------------------------
+set VS2008_DIR=%ProgramFiles%\Microsoft Visual Studio 9.0
+rem -------------------------------------
+
+rem -------------------------------------
+echo Microsoft Visual Studio 2008 directory: %VS2008_DIR%
+rem -------------------------------------
+
+rem -------------------------------------
+call "%VS2008_DIR%\VC\vcvarsall.bat" x86_amd64
+if errorlevel 1 goto exit
+rem -------------------------------------
+
+rem -------------------------------------
+set BOOST_ROOT_DIR=%~dp0
+set B2_BIN=%~dp0b2.exe
+set INSTALL_DIR=%~dp0..\boost-1.60.0-x64-vs2008
+rem -------------------------------------
+
+rem -------------------------------------
+echo Boost root directory    : %BOOST_ROOT_DIR%
+echo Boost.Build executable  : %B2_BIN%
+echo Install directory       : %INSTALL_DIR%
+rem -------------------------------------
+
+cd /d "%BOOST_ROOT_DIR%"
+"%B2_BIN%" -j2 --toolset=msvc-9.0 address-model=64 debug release link=static runtime-link=static threading=multi install --prefix="%INSTALL_DIR%"
+"%B2_BIN%" -j2 --toolset=msvc-9.0 address-model=64 debug release link=static runtime-link=shared threading=multi install --prefix="%INSTALL_DIR%"
+"%B2_BIN%" -j2 --toolset=msvc-9.0 address-model=64 debug release link=shared threading=multi install --prefix="%INSTALL_DIR%"
+
+:exit
