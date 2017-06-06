c7b3c72603c3dcc8a8d8e5e9e18fe6795d774f6b
Marat Abrarov <abrarov@gmail.com>
2017-06-06 17:33:10 +0300

    Added Docker image for building of Boost C++ Libraries with MinGW.

---
+++ docker/boost-mingw/app/bootstrap.bat
@@ -0,0 +1,15 @@
+@echo off
+
+REM
+REM Copyright (c) 2017 Marat Abrarov (abrarov@gmail.com)
+REM
+REM Distributed under the Boost Software License, Version 1.0. (See accompanying
+REM file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+REM
+
+set PATH=%PATH%;%MINGW_HOME%\bin
+if errorlevel 1 goto exit
+
+call "%BOOST_BOOTSTRAP%"
+
+:exit
