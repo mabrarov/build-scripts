356c7e1d9852d63f2268877271080d8f27be733e
Marat Abrarov <abrarov@gmail.com>
2017-06-15 00:42:13 +0300

    Removed excessive options from Docker image for building of OpenSSL by means of MinGW. Updated README for that Docker image.

--- docker/openssl-mingw/README.md
+++ docker/openssl-mingw/README.md
@@ -4,8 +4,16 @@ This docker image can be used for building of [OpenSSL](https://www.openssl.org/
 
 ## Examples of usage
 
-todo  
+Download source archive, build all combinations (x86, x64, shared and static libraries) and put results of build into 
+`C:\Users\Public\openssl-mingw\target` folder of Docker Host:  
 
 ```bash
-todo
+docker run --rm -v /C:/Users/Public/openssl-mingw/target:C:/target abrarov/openssl-mingw
+```
+
+Download source archive, build x64 shared libraries and put results of build into `C:\Users\Public\openssl-mingw\target` 
+folder of Docker Host:
+ 
+```bash
+docker run --rm -v /C:/Users/Public/openssl-mingw/target:C:/target -e OPENSSL_ADDRESS_MODEL=64 -e OPENSSL_LINKAGE=shared abrarov/openssl-mingw
 ```
--- docker/openssl-mingw/app/start.ps1
+++ docker/openssl-mingw/app/start.ps1
@@ -25,7 +25,6 @@ $openssl_download_url = "$env:OPENSSL_URL/openssl-$env:OPENSSL_VERSION.tar.gz"
 # Build OpenSSL
 $address_models = @("64", "32")
 $openssl_linkages = @("shared", "static")
-$runtime_linkages = @("shared", "static")
 
 # Limit build configurations if user asked for that
 if (Test-Path env:OPENSSL_ADDRESS_MODEL) {
@@ -33,9 +32,6 @@ if (Test-Path env:OPENSSL_ADDRESS_MODEL) {
 }
 if (Test-Path env:OPENSSL_LINKAGE) {
   $openssl_linkages = @("$env:OPENSSL_LINKAGE")
-}
-if (Test-Path env:OPENSSL_RUNTIME_LINKAGE) {
-  $runtime_linkages = @("$env:OPENSSL_RUNTIME_LINKAGE")
 }
 
 $openssl_downloaded = "False"
@@ -64,78 +60,65 @@ foreach ($address_model in $address_models) {
 
   foreach ($openssl_linkage in $openssl_linkages) {
     $env:OPENSSL_LINKAGE = $openssl_linkage
-    foreach ($runtime_linkage in $runtime_linkages) {
-      if ($runtime_linkage -eq "static" -and -not ($openssl_linkage -eq "static")) {
-        # Nothing to do with this type of configuration - just skip it
-        continue
-      }
-      if ($openssl_linkage -eq "static" -and -not ($runtime_linkage -eq "static")) {
-        # Nothing to do with this type of configuration - just skip it
-        continue
-      }
-      $env:OPENSSL_RUNTIME_LINKAGE = $runtime_linkage
+    $env:OPENSSL_BUILD_DIR="$env:BUILD_DIR\openssl-$env:OPENSSL_VERSION\$address_model\$env:OPENSSL_LINKAGE"
+    $env:OPENSSL_HOME="$env:OPENSSL_BUILD_DIR\openssl-$env:OPENSSL_VERSION"
+    Write-Host "Assuming root folder for sources is: $env:OPENSSL_HOME"
 
-      $env:OPENSSL_BUILD_DIR="$env:BUILD_DIR\openssl-$env:OPENSSL_VERSION\$address_model\$env:OPENSSL_LINKAGE"
-      $env:OPENSSL_HOME="$env:OPENSSL_BUILD_DIR\openssl-$env:OPENSSL_VERSION"
-      Write-Host "Assuming root folder for sources is: $env:OPENSSL_HOME"
-
-      if (Test-Path -Path "$env:OPENSSL_HOME") {
-        Write-Host "Found existing folder $env:OPENSSL_HOME, assuming that sources are in place and skipping downloading and unpacking of sources"
-      } else {
-        if ("$openssl_downloaded" -ne "True") {
-          if (Test-Path -Path "$openssl_archive_file") {
-            Write-Host "Found existing file $openssl_archive_file, assuming that sources are downloaded and skipping downloading of sources"
-          } else {
-            # Download OpenSSL
-            Write-Host "Downloading OpenSSL (source code archive) from: $openssl_download_url into: $openssl_archive_file"
-            (New-Object System.Net.WebClient).DownloadFile("$openssl_download_url", "$openssl_archive_file")
-          }
-          $openssl_downloaded = "True"
+    if (Test-Path -Path "$env:OPENSSL_HOME") {
+      Write-Host "Found existing folder $env:OPENSSL_HOME, assuming that sources are in place and skipping downloading and unpacking of sources"
+    } else {
+      if ("$openssl_downloaded" -ne "True") {
+        if (Test-Path -Path "$openssl_archive_file") {
+          Write-Host "Found existing file $openssl_archive_file, assuming that sources are downloaded and skipping downloading of sources"
+        } else {
+          # Download OpenSSL
+          Write-Host "Downloading OpenSSL (source code archive) from: $openssl_download_url into: $openssl_archive_file"
+          (New-Object System.Net.WebClient).DownloadFile("$openssl_download_url", "$openssl_archive_file")
         }
-
-        if (-not (Test-Path -Path "$env:OPENSSL_BUILD_DIR")) {
-          New-Item -path "$env:OPENSSL_BUILD_DIR" -type directory | out-null
-        }
-
-        # Unpack OpenSSL
-        Write-Host "Extracting source code archive from $openssl_archive_file to $env:OPENSSL_BUILD_DIR"
-        Set-Location -Path "$env:OPENSSL_BUILD_DIR"
-        $openssl_archive_msys_file = "$openssl_archive_file" -replace "\\", "/"
-        $openssl_archive_msys_file = "$openssl_archive_msys_file" -replace "^(C):", "/c"
-        & "tar.exe" xzf "$openssl_archive_msys_file"
-        if ($LastExitCode -ne 0) {
-          throw "Failed to extract OpenSSL from $openssl_archive_file to $env:OPENSSL_BUILD_DIR"
-        }
-        Write-Host "Extracted source code archive"
+        $openssl_downloaded = "True"
       }
 
-      $env:OPENSSL_INSTALL_DIR = "$env:TARGET_DIR\openssl-$env:OPENSSL_VERSION-$address_model_target_dir_suffix-mingw$mingw_version_suffix-$env:OPENSSL_LINKAGE"
-      $env:OPENSSL_STAGE_DIR = "$env:OPENSSL_HOME\dist"
-      $env:OPENSSL_STAGE_MSYS_DIR = "$env:OPENSSL_STAGE_DIR" -replace "\\", "/"
-      $env:OPENSSL_STAGE_MSYS_DIR = "$env:OPENSSL_STAGE_MSYS_DIR" -replace "^(C):", "/c"
+      if (-not (Test-Path -Path "$env:OPENSSL_BUILD_DIR")) {
+        New-Item -path "$env:OPENSSL_BUILD_DIR" -type directory | out-null
+      }
 
-      Set-Location -Path "$env:OPENSSL_HOME"
-
-      Write-Host "Building OpenSSL with theses parameters:"
-      Write-Host "MINGW_HOME              : $env:MINGW_HOME"
-      Write-Host "MSYS_HOME               : $env:MSYS_HOME"
-      Write-Host "OPENSSL_HOME            : $env:OPENSSL_HOME"
-      Write-Host "OPENSSL_INSTALL_DIR     : $env:OPENSSL_INSTALL_DIR"
-      Write-Host "OPENSSL_STAGE_DIR       : $env:OPENSSL_STAGE_DIR"
-      Write-Host "OPENSSL_STAGE_MSYS_DIR  : $env:OPENSSL_STAGE_MSYS_DIR"
-      Write-Host "OPENSSL_TOOLSET         : $env:OPENSSL_TOOLSET"
-      Write-Host "OPENSSL_ADDRESS_MODEL   : $env:OPENSSL_ADDRESS_MODEL"
-      Write-Host "OPENSSL_LINKAGE         : $env:OPENSSL_LINKAGE"
-      Write-Host "OPENSSL_RUNTIME_LINKAGE : $env:OPENSSL_RUNTIME_LINKAGE"
-
-      & "$env:SCRIPT_DIR\build.bat"
+      # Unpack OpenSSL
+      Write-Host "Extracting source code archive from $openssl_archive_file to $env:OPENSSL_BUILD_DIR"
+      Set-Location -Path "$env:OPENSSL_BUILD_DIR"
+      $openssl_archive_msys_file = "$openssl_archive_file" -replace "\\", "/"
+      $openssl_archive_msys_file = "$openssl_archive_msys_file" -replace "^(C):", "/c"
+      & "tar.exe" xzf "$openssl_archive_msys_file"
       if ($LastExitCode -ne 0) {
-        throw "Failed to build OpenSSL with OPENSSL_ADDRESS_MODEL = $env:OPENSSL_ADDRESS_MODEL, OPENSSL_LINKAGE = $env:OPENSSL_LINKAGE, OPENSSL_RUNTIME_LINKAGE = $env:OPENSSL_RUNTIME_LINKAGE"
+        throw "Failed to extract OpenSSL from $openssl_archive_file to $env:OPENSSL_BUILD_DIR"
       }
-
-      Write-Host "Copying built OpenSSL from $env:OPENSSL_STAGE_DIR to $env:OPENSSL_INSTALL_DIR"
-      Copy-Item -Force -Recurse -Path "$env:OPENSSL_STAGE_DIR" -Destination "$env:OPENSSL_INSTALL_DIR"
+      Write-Host "Extracted source code archive"
     }
+
+    $env:OPENSSL_INSTALL_DIR = "$env:TARGET_DIR\openssl-$env:OPENSSL_VERSION-$address_model_target_dir_suffix-mingw$mingw_version_suffix-$env:OPENSSL_LINKAGE"
+    $env:OPENSSL_STAGE_DIR = "$env:OPENSSL_HOME\dist"
+    $env:OPENSSL_STAGE_MSYS_DIR = "$env:OPENSSL_STAGE_DIR" -replace "\\", "/"
+    $env:OPENSSL_STAGE_MSYS_DIR = "$env:OPENSSL_STAGE_MSYS_DIR" -replace "^(C):", "/c"
+
+    Set-Location -Path "$env:OPENSSL_HOME"
+
+    Write-Host "Building OpenSSL with theses parameters:"
+    Write-Host "MINGW_HOME            : $env:MINGW_HOME"
+    Write-Host "MSYS_HOME             : $env:MSYS_HOME"
+    Write-Host "OPENSSL_HOME          : $env:OPENSSL_HOME"
+    Write-Host "OPENSSL_INSTALL_DIR   : $env:OPENSSL_INSTALL_DIR"
+    Write-Host "OPENSSL_STAGE_DIR     : $env:OPENSSL_STAGE_DIR"
+    Write-Host "OPENSSL_STAGE_MSYS_DIR: $env:OPENSSL_STAGE_MSYS_DIR"
+    Write-Host "OPENSSL_TOOLSET       : $env:OPENSSL_TOOLSET"
+    Write-Host "OPENSSL_ADDRESS_MODEL : $env:OPENSSL_ADDRESS_MODEL"
+    Write-Host "OPENSSL_LINKAGE       : $env:OPENSSL_LINKAGE"
+
+    & "$env:SCRIPT_DIR\build.bat"
+    if ($LastExitCode -ne 0) {
+      throw "Failed to build OpenSSL with OPENSSL_ADDRESS_MODEL = $env:OPENSSL_ADDRESS_MODEL, OPENSSL_LINKAGE = $env:OPENSSL_LINKAGE"
+    }
+
+    Write-Host "Copying built OpenSSL from $env:OPENSSL_STAGE_DIR to $env:OPENSSL_INSTALL_DIR"
+    Copy-Item -Force -Recurse -Path "$env:OPENSSL_STAGE_DIR" -Destination "$env:OPENSSL_INSTALL_DIR"
   }
 }
 
