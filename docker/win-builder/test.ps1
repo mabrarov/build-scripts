3abd265e9158613dd5690f617b0981869252f3ec
Marat Abrarov <abrarov@gmail.com>
2019-02-15 02:23:31 +0300

    Fixed tests for win-builder and mingw docker images

--- docker/win-builder/test.ps1
+++ docker/win-builder/test.ps1
@@ -8,13 +8,13 @@
 $ErrorActionPreference = "Stop"
 
 Write-Host "Running 7-Zip in container created from abrarov/win-builder:latest image"
-docker run --rm abrarov/win-builder:latest "%SEVEN_ZIP_HOME%/7z.exe"
+docker run --rm abrarov/win-builder:latest "C:\Program Files\7-Zip\7z.exe"
 
 Write-Host "Running MSYS2 GNU make in container created from abrarov/win-builder:latest image"
-docker run --rm abrarov/win-builder:latest "%MSYS_HOME%\usr\bin\make" --version
+docker run --rm abrarov/win-builder:latest "C:\msys64\usr\bin\make" --version
 
 Write-Host "Running ActivePerl in container created from abrarov/win-builder:latest image"
-docker run --rm abrarov/win-builder:latest "%ACTIVE_PERL_HOME%\bin\perl.exe" --version
+docker run --rm abrarov/win-builder:latest "C:\Perl64\bin\perl.exe" --version
 
 Write-Host "Running Python in container created from abrarov/win-builder:latest image"
-docker run --rm abrarov/win-builder:latest "%PYTHON_HOME%\bin\python.exe" --version
+docker run --rm abrarov/win-builder:latest "C:\Program Files\Python37\bin\python.exe" --version
