90c8d9daee4458112b089a00c4d176fe9cbdf7e4
Marat Abrarov <abrarov@gmail.com>
2019-08-15 18:11:49 +0300

    Workaround for AppVeyor issue with 7-Zip in abrarov/win-builder Docker image

---
+++ docker/win-builder/app/install1.ps1
@@ -0,0 +1,23 @@
+#
+# Copyright (c) 2017 Marat Abrarov (abrarov@gmail.com)
+#
+# Distributed under the MIT License (see accompanying LICENSE)
+#
+
+# Stop immediately if any error happens
+$ErrorActionPreference = "Stop"
+
+# Enable all versions of TLS
+[System.Net.ServicePointManager]::SecurityProtocol = @("Tls12","Tls11","Tls","Ssl3")
+
+# Download and install lZ-Zip
+$app_dir = "C:\app"
+$seven_zip_dist_name = "7z${env:SEVEN_ZIP_VERSION}-x64.msi"
+$seven_zip_dist = "${app_dir}\${seven_zip_dist_name}"
+if (-not (Test-Path -Path "${seven_zip_dist}")) {
+  $seven_zip_url = "${env:SEVEN_ZIP_DOWNLOAD_URL}/${seven_zip_dist_name}"
+  Write-Host "Downloading Z-Zip from ${seven_zip_url} into ${seven_zip_dist_name}"
+  (New-Object System.Net.WebClient).DownloadFile("${seven_zip_url}", "${seven_zip_dist_name}")
+}
+Write-Host "Installing Z-Zip from ${seven_zip_dist} into ${env:SEVEN_ZIP_HOME}"
+Start-Process -FilePath msiexec -ArgumentList ("/package", "${seven_zip_dist}", "/quiet", "/qn", "/norestart") -Wait
--- docker/win-builder/app/install2.ps1
+++ docker/win-builder/app/install2.ps1
@@ -10,19 +10,8 @@ $ErrorActionPreference = "Stop"
 # Enable all versions of TLS
 [System.Net.ServicePointManager]::SecurityProtocol = @("Tls12","Tls11","Tls","Ssl3")
 
-# Download and install lZ-Zip
-$app_dir = "C:\app"
-$seven_zip_dist_name = "7z${env:SEVEN_ZIP_VERSION}-x64.msi"
-$seven_zip_dist = "${app_dir}\${seven_zip_dist_name}"
-if (-not (Test-Path -Path "${seven_zip_dist}")) {
-  $seven_zip_url = "${env:SEVEN_ZIP_DOWNLOAD_URL}/${seven_zip_dist_name}"
-  Write-Host "Downloading Z-Zip from ${seven_zip_url} into ${seven_zip_dist_name}"
-  (New-Object System.Net.WebClient).DownloadFile("${seven_zip_url}", "${seven_zip_dist_name}")
-}
-Write-Host "Installing Z-Zip from ${seven_zip_dist} into ${env:SEVEN_ZIP_HOME}"
-Start-Process -FilePath msiexec -ArgumentList ("/package", "${seven_zip_dist}", "/quiet", "/qn", "/norestart") -Wait
-
 # Download and install MSYS2
+$app_dir = "C:\app"
 $msys_tar_filename = "msys2-base-${env:MSYS2_TARGET}-${env:MSYS2_VERSION}.tar"
 $msys_dist_filename = "${msys_tar_filename}.xz"
 $msys_url = "${env:MSYS2_URL}/${env:MSYS2_TARGET}/${msys_dist_filename}"
@@ -32,7 +21,7 @@ Write-Host "Downloading MSYS2 x64 from ${msys_url} into ${msys_dist}"
 Write-Host "Installing MSYS2 x64 into ${env:MSYS_HOME}"
 & "${env:SEVEN_ZIP_HOME}\7z.exe" x "${msys_dist}" -o"${env:TMP}" -aoa -y -bd | out-null
 & "${env:SEVEN_ZIP_HOME}\7z.exe" x "${env:TMP}\${msys_tar_filename}" -o"C:" -aoa -y -bd | out-null
-#& "C:\app\msys2.bat"
+& "${app_dir}\msys2.bat"
 
 # Download and install ActivePerl
 $active_perl_url = "${env:ACTIVE_PERL_URL}/${env:ACTIVE_PERL_VERSION}/ActivePerl-${env:ACTIVE_PERL_VERSION}-MSWin32-x64-${env:ACTIVE_PERL_BUILD}.exe"
