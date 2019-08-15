079bcc5cbb35927a00f9f76a49c5e993845d7268
Marat Abrarov <abrarov@gmail.com>
2019-08-15 17:35:33 +0300

    Minor log message enhancement

--- docker/win-builder/app/msys2.bat
+++ docker/win-builder/app/msys2.bat
@@ -10,12 +10,12 @@ set PATH=%MSYS_HOME%\usr\bin;%PATH%
 
 set exit_code=0
 
-echo Initializing MSYS package manager
+echo Initializing MSYS2
 echo exit | "%MSYS_HOME%\msys2_shell.cmd" -no-start -defterm
 set exit_code=%errorlevel%
 if %exit_code% neq 0 goto exit
 
-echo Installing pacman base-devel package group (MSYS development tools)
+echo Installing pacman base-devel package group (MSYS2 development tools)
 pacman -S --needed --noconfirm base-devel
 set exit_code=%errorlevel%
 if %exit_code% neq 0 goto exit
