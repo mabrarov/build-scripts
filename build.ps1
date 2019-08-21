76560538078156f6adb15237c6e6229fa3b37a28
Marat Abrarov <abrarov@gmail.com>
2019-08-21 15:00:26 +0300

    Building of boost-msvc-2015, icu-msvc-2015 and openssl-msvc-2015 Docker images in CI scripts

--- build.ps1
+++ build.ps1
@@ -17,7 +17,10 @@ $dirs = @(
   "docker/icu-msvc-2017",
   "docker/openssl-mingw",
   "docker/openssl-msvc-2017",
-  "docker/msvc-2015"
+  "docker/msvc-2015",
+  "docker/boost-msvc-2015",
+  "docker/icu-msvc-2015",
+  "docker/openssl-msvc-2015"
 )
 
 ${dirs}.GetEnumerator() | ForEach-Object {
