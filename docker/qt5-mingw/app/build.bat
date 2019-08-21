956de809a189ca379bbb807e185c621597333084
Marat Abrarov <abrarov@gmail.com>
2019-08-21 13:03:23 +0300

    Fixed formatting of scripts of qt5-mingw Docker image

--- docker/qt5-mingw/app/build.bat
+++ docker/qt5-mingw/app/build.bat
@@ -24,18 +24,18 @@ if not "--%QT_PATCH_MSYS_FILE%" == "--" (
 )
 
 call "configure.bat" ^
--platform win32-g++ ^
--debug-and-release ^
--opensource -confirm-license ^
-%QT_CONFIGURE_OPTIONS_LINKAGE% ^
--nomake examples ^
--nomake tests ^
-%QT_CONFIGURE_OPTIONS% ^
--I "%OPENSSL_DIR%\include" ^
--L "%OPENSSL_DIR%\lib" ^
--I "%ICU_DIR%\include" ^
--L "%ICU_DIR%\lib" ^
--prefix "%QT_INSTALL_DIR%"
+  -platform win32-g++ ^
+  -debug-and-release ^
+  -opensource -confirm-license ^
+  "%QT_CONFIGURE_OPTIONS_LINKAGE%" ^
+  -nomake examples ^
+  -nomake tests ^
+  %QT_CONFIGURE_OPTIONS% ^
+  -I "%OPENSSL_DIR%\include" ^
+  -L "%OPENSSL_DIR%\lib" ^
+  -I "%ICU_DIR%\include" ^
+  -L "%ICU_DIR%\lib" ^
+  -prefix "%QT_INSTALL_DIR%"
 set exit_code=%errorlevel%
 if %exit_code% neq 0 goto exit
 
