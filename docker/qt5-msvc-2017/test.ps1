7458cf5a0c461ab87892dd9b3820bf959eda8dc4
Marat Abrarov <abrarov@gmail.com>
2019-08-24 01:38:32 +0300

    Fixed CI scripts for qt5-msvc-2017 Docker image to follow existing approach

--- docker/qt5-msvc-2017/test.ps1
+++ docker/qt5-msvc-2017/test.ps1
@@ -9,14 +9,4 @@ $ErrorActionPreference = "Stop"
 
 $image_repository = "abrarov/$(Split-Path "${PSScriptRoot}" -Leaf)"
 
-Write-Host "Running jom in container created from ${image_repository}:latest image"
-docker run --rm "${image_repository}:latest" "C:\jom\jom" -version
-if (${LastExitCode} -ne 0) {
-  throw "Failed to get version of jom"
-}
-
-Write-Host "Running Clang in container created from ${image_repository}:latest image"
-docker run --rm "${image_repository}:latest" "& 'C:\Program Files\LLVM\bin\clang++.exe' --version"
-if (${LastExitCode} -ne 0) {
-  throw "Failed to get version of Clang"
-}
+#TODO: add some tests
