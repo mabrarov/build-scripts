a3e885892812dc9f3fcc1dbf644cc9b59f056cc1
Marat Abrarov <abrarov@gmail.com>
2019-08-24 16:57:12 +0300

    Workaround for Travis

---
+++ build_and_test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#
+# Copyright (c) 2019 Marat Abrarov (abrarov@gmail.com)
+#
+# Distributed under the MIT License (see accompanying LICENSE)
+#
+
+set -e
+
+this_path="$(cd "$(dirname "$0")" && pwd)"
+
+# shellcheck source=travis/travis_jigger.sh
+source "${this_path}/travis/travis_jigger.sh"
+
+# shellcheck source=travis/travis_wait.sh
+source "${this_path}/travis/travis_wait.sh"
+
+# Full build disabled due to testing and resource consumption
+# powershell -ExecutionPolicy Bypass -File "${this_path}/build.ps1"
+travis_wait 120 powershell -ExecutionPolicy Bypass -File "${this_path}/docker/win-builder/build.ps1"
+
+#powershell -ExecutionPolicy Bypass -File "${this_path}/test.ps1"
+travis_wait 20 powershell -ExecutionPolicy Bypass -File "${this_path}/docker/win-builder/test.ps1"
---
+++ travis/travis_wait.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+
+# Copied from https://github.com/travis-ci/travis-build/blob/master/lib/travis/build/bash/travis_wait.bash
+travis_wait() {
+  local timeout="${1}"
+
+  if [[ "${timeout}" =~ ^[0-9]+$ ]]; then
+    shift
+  else
+    timeout=20
+  fi
+
+  local cmd=("${@}")
+
+  "${cmd[@]}" &
+  local cmd_pid="${!}"
+
+  travis_jigger "${!}" "${timeout}" "${cmd[@]}" &
+  local jigger_pid="${!}"
+  local result
+
+  {
+    wait "${cmd_pid}" 2>/dev/null
+    result="${?}"
+    ps -p"${jigger_pid}" &>/dev/null && kill "${jigger_pid}"
+  }
+
+  if [[ "${result}" -eq 0 ]]; then
+    echo -e "\\n${ANSI_GREEN}The command ${cmd[*]} exited with ${result}.${ANSI_RESET}"
+  else
+    echo -e "\\n${ANSI_RED}The command ${cmd[*]} exited with ${result}.${ANSI_RESET}"
+  fi
+
+  return "${result}"
+}
