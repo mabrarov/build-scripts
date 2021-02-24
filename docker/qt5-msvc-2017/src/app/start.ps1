b86a9c9baa7af9389f26bdd1b9cf25e375179b62
Marat Abrarov <abrarov@gmail.com>
2021-02-24 20:11:35 +0300

    Qt 5.15.2

--- docker/qt5-msvc-2017/src/app/start.ps1
+++ docker/qt5-msvc-2017/src/app/start.ps1
@@ -42,10 +42,7 @@ if (Test-Path env:QT_LINKAGE) {
 if (!(Test-Path env:QT_CONFIGURE_OPTIONS)) {
   $env:QT_CONFIGURE_OPTIONS = "-opengl desktop -icu"
   switch (${env:QT_VERSION}) {
-    "5.13.0" {
-      # todo
-    }
-    "5.13.1" {
+    "5.15.2" {
       # todo
     }
     default {
